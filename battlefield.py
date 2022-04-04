e401ebf510e2ec0614fea97a573dd731acc2b596
alex-colvin <alexcolvin@live.com>
2022-04-04 10:52:08 -0400

    init

---
+++ battlefield.py
@@ -0,0 +1,17 @@
+class Battlefield:
+
+    def __init__(self) -> None:
+        self.robot = Robot
+        self.dinosaur = Dinosaur
+    
+    def run_game (self):
+        pass
+
+    def display_welcome (self):
+        pass
+
+    def battle_phase (self):
+        pass
+    
+    def display_winner (self):
+        pass
