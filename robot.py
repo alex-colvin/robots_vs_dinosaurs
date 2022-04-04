fb64bbfeceefb7d82f803badd980c543eb38df71
alex-colvin <alexcolvin@live.com>
2022-04-04 11:01:27 -0400

    created Dinosaur class

--- robot.py
+++ robot.py
@@ -1,9 +1,11 @@
+from weapon import Weapon
+
 class Robot:
     
     def __init__(self, name) -> None:
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon
+        self.active_weapon = Weapon("Laser Sword", 25)
 
     def attack(self, dinosaur):
         pass
